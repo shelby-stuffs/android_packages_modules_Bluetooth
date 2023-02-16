970b244b80adec32c0af53d5db2707bc73f3a1a0
manjula <quic_manjula@quicinc.com>
2023-02-16 16:10:23 +0530

    Add missing changes to bind to HeadsetService
    Headset profile binding to BluetoothProfileConnector.
    and get HeadsetService instance while calling SDK APIs bind to Apps
    
    Change-Id: I61db92f3608f068d615854b45c1cec3009a98e12
    CRs-Fixed: 3394544

--- framework/java/android/bluetooth/BluetoothHeadset.java
+++ framework/java/android/bluetooth/BluetoothHeadset.java
@@ -383,8 +383,6 @@ public final class BluetoothHeadset implements BluetoothProfile {
             "android.bluetooth.headset.extra.HF_INDICATORS_IND_VALUE";
 
     private final ReentrantReadWriteLock mServiceLock = new ReentrantReadWriteLock();
-
-    @GuardedBy("mServiceLock") private IBluetoothHeadset mService;
     private final BluetoothAdapter mAdapter;
     private final AttributionSource mAttributionSource;
     private final BluetoothProfileConnector<IBluetoothHeadset> mProfileConnector =
@@ -403,6 +401,7 @@ public final class BluetoothHeadset implements BluetoothProfile {
             BluetoothAdapter adapter) {
         mAdapter = adapter;
         mAttributionSource = adapter.getAttributionSource();
+        mProfileConnector.connect(context, listener);
     }
 
     /**
@@ -527,7 +526,7 @@ public final class BluetoothHeadset implements BluetoothProfile {
         final List<BluetoothDevice> defaultValue = new ArrayList<BluetoothDevice>();
         try {
             mServiceLock.readLock().lock();
-            final IBluetoothHeadset service = mService;
+            final IBluetoothHeadset service = getService();
             if (service == null) {
                 Log.w(TAG, "Proxy not attached to service");
                 if (DBG) log(Log.getStackTraceString(new Throwable()));
@@ -1502,7 +1501,7 @@ public final class BluetoothHeadset implements BluetoothProfile {
     })
     public void phoneStateChangedDsDa(int numActive, int numHeld, int callState, String number,
             int type, String name) {
-        final IBluetoothHeadset service = mService;
+        final IBluetoothHeadset service = getService();
         if (service != null && isEnabled()) {
             try {
                 service.phoneStateChangedDsDa(numActive, numHeld, callState, number, type, name,
@@ -1528,7 +1527,7 @@ public final class BluetoothHeadset implements BluetoothProfile {
     })
     public void clccResponseDsDa(int index, int direction, int status, int mode, boolean mpty,
             String number, int type) {
-        final IBluetoothHeadset service = mService;
+        final IBluetoothHeadset service = getService();
         if (service != null && isEnabled()) {
             try {
                 service.clccResponseDsDa(index, direction, status, mode, mpty, number, type,
